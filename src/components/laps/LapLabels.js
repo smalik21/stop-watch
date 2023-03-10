f2cef03c7e176daeb52aa9613e53546796f10a16
Sahil Malik <sahilmalik25007@gmail.com>
2023-03-10 17:23:48 +0530

    Updates

--- src/components/laps/LapLabels.js
+++
@@ -1,9 +0,0 @@
-export default function LapLabels(props) {
-    return (
-        <div className="lap-labels">
-            <p className="lap-label">Lap</p>
-            <p className="lap-times-label">Lap Times</p>
-            <p className="overall-time-label">Overall Time</p>
-        </div>
-    );
-}
